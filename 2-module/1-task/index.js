94d321279dfba053245a1c1022eceb0d8f1096ce
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-07-13 22:01:05 +0300

    Homework 2 - 3 задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+    let summa=0;
+    for (let key in salaries){
+        if (typeof salaries[key] === 'number' && Number.isFinite(salaries[key]) )    {
+            summa += salaries[key];
+        }
+    }
+    return summa;
 }
