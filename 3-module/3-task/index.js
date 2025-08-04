daa856d522376c28934bc4a06da63714661aeb29
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-08-04 20:13:20 +0300

    Homework 3 - 5 задач

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let liters = [];
+  liters = str.split("-");
+  let upperStr = liters[0];
+  for (let i = 1; i < liters.length; i += 1) {
+      upperStr = liters[i];
+      upperStr = upperStr[0].toUpperCase() + upperStr.slice(1);
+      liters[i] = upperStr;
+  } 
+  return liters.join("");
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,7 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(users,age) {
+let result = '';
+for (let i=0; i<users.length ; i+=1) {
+if (users[i].age<=age) {result = result + users[i].name + ', ' + users[i].balance + '\n'} 
+}
+return result.substr(0,result.length-1)
 }
