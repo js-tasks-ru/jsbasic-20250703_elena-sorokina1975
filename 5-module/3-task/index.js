b7b25f19e4c5f450e570b475f4025275f84cc8f0
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-08-10 18:44:40 +0300

    Homework 5 - 3 задачи

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const countSlide = 4;  // кол-во слайдов в карусели
+  const buttonArrowRight = document.querySelector('.carousel__arrow_right') ;
+  const buttonArrowLeft = document.querySelector('.carousel__arrow_left') ;
+  const inner = document.querySelector('.carousel__inner') ;  
+  let i = 0;
+  
+  buttonArrowLeft.style.display = 'none';
+
+  function paint(i){
+    inner.style.transform = `translateX(-${(i*inner.offsetWidth)}px)`;
+    
+    if (i == countSlide-1) {
+        buttonArrowRight.style.display = 'none';
+      } else {
+        buttonArrowRight.style.display = '';
+      }    
+    if (i == 0) {
+        buttonArrowLeft.style.display = 'none';
+      } else {
+        buttonArrowLeft.style.display = '';
+      } };
+
+  buttonArrowRight.onclick = function(event) { 
+      i++;  
+      if (i > countSlide-1) {i = countSlide-1};
+      paint(i);
+    }  
+
+  buttonArrowLeft.onclick = function(event) {  
+       i--; 
+       if (i < 0 ) {i = 0}
+       paint(i);
+      }  
+
+      
 }
